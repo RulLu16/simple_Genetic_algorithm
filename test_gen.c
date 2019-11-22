f9c596b1e29127a7eb421a24bf5dbb8c8cff4863
YeEunLee <37532204+RulLu16@users.noreply.github.com>
2019-11-22 15:03:57 +0900

    first_refactoring

--- test_gen.c
+++ test_gen.c
@@ -14,17 +14,20 @@ void exchange(struct gen*, struct gen*);
 int* choose(struct gen*);
 void mutant(struct gen*);
 int fitness(int*, struct gen*, int*);
+void PrintResult(struct gen*, int, int);
+void GivePortion(struct gen*, float*);
+int GoRoulette(float*);
+void ChildToParent(struct gen*, struct gen*);
 
 int main(){
 	int password[6];
-	struct gen current_gen[6];
-	struct gen next_gen[6];
-	int i,j;
 	int prop;
 	int gen_num=0;
 	int find=-1;
-	int temp;
+	int temp,i,j;
 	int password_has_num[10] = { 0,0,0,0,0,0,0,0,0,0 };
+	struct gen current_gen[6];
+	struct gen next_gen[6];
 	srand(time(NULL));
 	printf("Enter your password (6 numbers) : ");
 	
@@ -32,7 +35,7 @@ int main(){
 		scanf("%1d",&password[i]);
 		temp = password[i];
 		password_has_num[temp] = 1;
-	}
+	} //scan password
 	
 	for(i=0;i<6;i++){
 		for(j=0;j<6;j++){
@@ -40,101 +43,59 @@ int main(){
 			current_gen[i].word[j]=prop;
 		}
 		current_gen[i].fit=0;
-	}
+	}//creat random generation
 	
 	while(1){
 		find=fitness(password, current_gen,password_has_num);
 		
 		if(find>-1)
 			break;
-		
-		/*for (i = 0; i < 6; i++) {
-			for (j = 0; j < 6; j++)
-				printf("%d", current_gen[i].word[j]);
-			printf("\n");
-		}*/
 
 		exchange(current_gen, next_gen);
 		gen_num++;
-		//printf("%d -------------------\n", gen_num);
-		
 		
 	}
-	
-		printf("Find it!! Your pass word is ");
-		for(i=0;i<6;i++)
-			printf("%d",current_gen[find].word[i]);
-		printf("!\n");
-		printf("To find out, use %d generation.\n",gen_num);
+
+	PrintResult(current_gen,gen_num, find);
 	
 	return 0;
 } 
 
 int* choose(struct gen* p){
-	int* choice;
-	float portion[6];
-	int roulette;
-	int sum=0;
+	int choice[2];
 	int i,j;
-	int flag = 0;
+	int temp;
+	float portion[6];
 	
-	choice=(int*)malloc(sizeof(int)*2);
+	GivePortion(p,portion);
 	
-	for(i=0;i<6;i++){
-			sum=sum+p[i].fit;
-	}
-	for(i=0;i<6;i++){
-		portion[i]=(p[i].fit * 100 / sum);
-	}
-	//srand(time(NULL));
-	roulette=rand()%100;
-	sum=0; 
-	for(i=0;i<5;i++){
-		sum = sum + portion[i];
-		if(roulette<sum){
-			flag = 1;
-			choice[0]=i;
-			break;
-		}
-	}
-	if (flag == 0)
-		choice[0] = 5;
-
+	choice[0]=GoRoulette(portion);
+	
 	while (1) {
-		roulette=rand()%100;
-		flag = 0;
-		sum = 0;
-		for(i=0;i<5;i++){
-			sum=sum + portion[i];
-			if(roulette<sum){
-				flag = 1;
-				choice[1]=i;
-				break;
-			}
-		}
-		if (flag == 0)
-			choice[1] = 5;
-
-		if(choice[0]==choice[1])
-			continue;
-		else
+	
+		temp=GoRoulette(portion);
+		
+		if(choice[0]!=temp){
+			choice[1]=temp;
 			break;
+		}
+			
 	}
+	
 	return choice;
 }   
 
 void exchange(struct gen* p, struct gen* n){
-	int* choice;
+	int* parent;
 	int i,j;
 	int first, second;
 	int temp;
 
 
 	for(i=0;i<6;i++){
-		choice=choose(p);
-		//printf("%d, %d\n", choice[0], choice[1]);
-		first = choice[0];
-		second = choice[1];
+		parent=choose(p);
+		first = parent[0];
+		second = parent[1];
 		/*for (j = 0; j < 6; j++) {
 			temp   = rand() % 2;
 			if (temp == 1) {
@@ -143,24 +104,16 @@ void exchange(struct gen* p, struct gen* n){
 			else
 				n[i].word[j] = p[first].word[j];
 		}*/
-		for(j=0;j<4;j++){
-			first = choice[0];
+		for(j=0;j<4;j++)
 			n[i].word[j]=p[first].word[j];
-		}
-		for(j=4;j<6;j++){
-			second = choice[1];
+		
+		for(j=4;j<6;j++)
 			n[i].word[j]=p[second].word[j];
-		}
 	}
 
 	mutant(n);
 
-	for(i=0;i<6;i++){
-		for(j=0;j<6;j++){
-			p[i].word[j]=n[i].word[j];
-		}
-		p[i].fit=0;
-	}
+	ChildToParent(p,n);
 	
 }
 
@@ -170,13 +123,12 @@ void mutant(struct gen* n){
 	//srand(time(NULL));
 	
 	for(i=0;i<6;i++){
-			for (j = 0; j < 6; j++) {
-				mutant = rand() % 100;
-				if (mutant == 1) {
-					n[i].word[j] = rand() % 10;
-				}
+		for (j = 0; j < 6; j++) {
+			mutant = rand() % 100;
+			if (mutant == 1) {
+				n[i].word[j] = rand() % 10;
 			}
-		
+		}	
 	}
 }
 
@@ -198,13 +150,62 @@ int fitness(int* p, struct gen* g,int* num){
 		}
 	}
 
-	/*for (i = 0; i < 6; i++)
-		printf("%d\n", g[i].fit);*/
-
 	for(i=0;i<6;i++){
 		if(g[i].fit==72){
 			return i;
 		}
 	}
 	return -1;
+}
+
+void PrintResult(struct gen* result_gen, int gen_num, int find){
+	int i,j;
+	
+	printf("Find it!! Your pass word is ");
+	for(i=0;i<6;i++)
+		printf("%d",result_gen[find].word[i]);
+	printf("!\n");
+	printf("To find out, use %d generation.\n",gen_num);
+}
+
+void GivePortion(struct gen* p, float* portion){
+	int i;
+	int sum=0;
+	
+	for(i=0;i<6;i++){
+			sum=sum+p[i].fit;
+	}
+	
+	for(i=0;i<6;i++){
+		portion[i]=(p[i].fit * 100 / sum);
+	}
+	
+}
+
+int GoRoulette(float* portion){
+	int roulette_point, i;
+	int sum=0;
+	
+	roulette_point=rand()%100;
+	
+	for(i=0;i<5;i++){
+		sum = sum + portion[i];
+		
+		if(roulette_point<sum)
+			return i;
+	}
+	
+	return 5;
+}
+
+void ChildToParent(struct gen* p, struct gen* n){
+	int i,j;
+	
+	for(i=0;i<6;i++){
+		for(j=0;j<6;j++){
+			p[i].word[j]=n[i].word[j];
+		}
+		p[i].fit=0;
+	}
+	
 }
